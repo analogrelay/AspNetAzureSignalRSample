26698e4022c66d39ccf989268d34d563d89f6cbb
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-09-25 11:38:20 -0700

    update to use service

--- AspNetAzureSignalRSample/Startup.cs
+++ AspNetAzureSignalRSample/Startup.cs
@@ -10,7 +10,7 @@ namespace AspNetAzureSignalRSample
         public void Configuration(IAppBuilder app)
         {
             // Any connection or hub wire up and configuration should go here
-            app.MapSignalR();
+            app.MapAzureSignalR(typeof(Startup).FullName);
         }
     }
 }
